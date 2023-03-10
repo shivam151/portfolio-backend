b4911b5724754e49627b232d407952769ca04404
shivam151 <shivamkati45@gmail.com>
2023-03-10 20:07:17 +0530

    yes

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,27 @@ const Message = require('../models/Message')
 
 
 router.post('/msg', async (req, res) => {
-    const { name, email, message, subject } = req.body;
-    if (!email || !message || !subject || !name) {
-        res.status(402).json({ error: "Data is missing" });
+    try {
+        const { name, email, message, subject } = req.body;
+        if (!email || !message || !subject || !name) {
+            res.status(402).json({ error: "Data is missing" });
+        }
+
+        const messgae = await Message.create({
+            name: name,
+            email: email,
+            message: message,
+            subject: subject,
+        }).then((u) => {
+            console.log("Message created Succesfully");
+            res.status(200).send({ success: true });
+        }).catch((err) => {
+            res.status(500).send({ success: false, errmsg: err });
+        })
+    } catch (err) {
+        res.status(500).send({ success: false, errmsg: err });
     }
 
-    const messgae = await Message.create({
-        name: name,
-        email: email,
-        message: message,
-        subject: subject,
-    }).then((u) => {
-        console.log("Message created Succesfully");
-        res.status(200).send({ success: true });
-    }).catch((err) => {
-        res.status(500).send({ success: false, errmsg: err });
-    })
-
-    const msg = new Messgae.create({
-        email, message, subject, name
-    })
-
-    await msg.save();
-    res.status(201).send({ "Success": true })
 })
 
 
